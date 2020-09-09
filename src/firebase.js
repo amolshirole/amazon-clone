c18db0e982243c4a2dcc288c6ab713cfdb887cfb
amolshirole <69356860+amolshirole@users.noreply.github.com>
2020-09-09 12:18:35 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBuTeygbeFwLiYID_ml0Lb97ojMwdVg-OM",
+  authDomain: "clone-30661.firebaseapp.com",
+  databaseURL: "https://clone-30661.firebaseio.com",
+  projectId: "clone-30661",
+  storageBucket: "clone-30661.appspot.com",
+  messagingSenderId: "461562796307",
+  appId: "1:461562796307:web:0cf85d206a251b814f22e6",
+  measurementId: "G-WJ74SZN0DT",
+};
